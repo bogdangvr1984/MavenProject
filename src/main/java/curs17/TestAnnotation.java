044b37ae9d16fe75262771d92fdfe8cd566f52ff
unknown <gavriluta@gmail.com>
2022-11-03 21:38:32 +0200

    curs17

---
+++ src/main/java/curs17/TestAnnotation.java
@@ -0,0 +1,25 @@
+package curs17;
+
+import org.testng.annotations.Test;
+
+@Test
+public class TestAnnotation {
+	
+//	@Test
+	public void test1() {
+		System.out.println("Test 1");
+	}
+	
+//	@Test
+	public void test2() {
+		System.out.println("Test 2");
+	}
+
+	
+	@Test
+	private void test3() {
+		System.out.println("Test 3");
+	}
+
+
+}
