926280805dba90ba2ef3ffbf5f56f0935ffe4593
unknown <gavriluta@gmail.com>
2022-11-17 21:32:26 +0200

    curs21

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -16,7 +18,11 @@ public class BaseTest {
 		driver = WebDriverManager.chromedriver().create();
 		driver.get("https://keybooks.ro");
 		//driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+		
+		//driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+		//driver.get("https://the-internet.herokuapp.com/dynamic_loading/2");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
 	}
 	
